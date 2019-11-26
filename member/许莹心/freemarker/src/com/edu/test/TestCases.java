fe0c741a71af0107614f9fc31c4d0a108f9e0d91
xuyingxin316 <1670282360@qq.com>
2019-11-26 10:31:37 +0800

    learn

---
+++ member/许莹心/freemarker/src/com/edu/test/TestCases.java
@@ -0,0 +1,12 @@
+package com.edu.test;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+public class TestCases {
+	@Test
+	public void test1(){
+		System.out.println("hello");
+	}
+	
+}
